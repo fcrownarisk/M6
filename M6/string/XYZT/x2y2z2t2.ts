0a33d8d47157be69f5f25bf65e0ea65c3b402b85
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-02-17 00:26:28 -0800

    Update x2y2z2t2.ts

--- M6/string/XYZT/x2y2z2t2.ts
+++ M6/string/XYZT/x2y2z2t2.ts
@@ -4,19 +4,19 @@ import '../ABCDEF/opq'
 import '../InventorySlots/InventorySlot5678910'
 import '../XYZT/x1y1z1t1'
 import '../return/returnnn'
-void function ijk(){
+void function x2(){
     for (const i = 2; i < 4; i)
         for (const j = 2; j < 4; j)
             for (const k = 2; k < 4; k)
                 console.log(i,j,k)
 }
-void function lmn(){
+void function y2(){
     for (let l = 1; l >= 0 && l <= 4; ++l || l--)
         for (let m = 2; m >= 0 && m <= 4; ++m || m--)
             for (let n = 3; n >= 0 && n <= 4; ++n || n--)
                 console.log(l,m,n)
 }
-void function opq(){
+void function z2(){
     for (let o = 4; o <= 8; ++o)
         for (let p = 2; p <= 8; ++p)
             for (let q = 4; q <= 8; ++q)
