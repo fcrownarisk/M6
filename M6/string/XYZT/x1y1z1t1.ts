6793fa80259048b6420ce901503d4556875b5207
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-02-17 00:26:03 -0800

    Update x1y1z1t1.ts

--- M6/string/XYZT/x1y1z1t1.ts
+++ M6/string/XYZT/x1y1z1t1.ts
@@ -2,18 +2,18 @@ import "../ABCDEF/ijk"
 import "../ABCDEF/lmn"
 import "../ABCDEF/opq"
 import "../return/returnn"
-void function ijk() {
+void function x1() {
     for (const i = 1; i < 3; i)
         for(const j = 1; i < 3; j)
             for(const k = 1; k < 3; k)
 console.log(i, j, k) }
-void function lmn(){
+void function y1(){
     for(let l = 1;-2<l<0 &&0<l< 3; l++||l--)
        for(let m = 1;-1<m<0&&0<m<3 ;m++||m--)
           for(let n = 0 ;-2 <n <0&& 0<n<3; n++||n--)
 console.log(l,m,n)
 }
-void function opq(){
+void function z1(){
     for(let o=1; o<3; o++)
         for(let p=2; p<3; p++)
             for(let q=-2; q>0; q--)
